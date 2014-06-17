32e62bd376b690f66fa1db56b8ced86c11e29625
junkyartist <harsh.nsit2007@gmail.com>
2014-06-17 11:56:43 +0530

    ex1 draft
    first commit compute cost function working

---
+++ mlclass-ex1/computeCost.m
@@ -0,0 +1,31 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+j = zeros(m,1);
+x = X(:,2);
+for i=1:m
+	j(i) = y(i) - theta(1) - x(i)*theta(2);
+	j(i) = square(j(i));
+end
+
+j=j';
+J=sum(j)/(2*m);
+% =========================================================================
+
+
+
+
+function [x] = square(x)
+	x =x*x;
+
